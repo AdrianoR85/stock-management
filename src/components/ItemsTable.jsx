1bac083996c8e9889401b32a4d6596ae68eb1551
AdrianoR85 <adrianorosa085@gmail.com>
2024-05-19 21:45:55 -0300

    Created table

--- src/components/ItemsTable.jsx
+++ src/components/ItemsTable.jsx
@@ -1,16 +1,29 @@
+import { Link } from "react-router-dom";
+
 export default function ItemsTable() {
   return (
     <table>
       <thead>
         <tr>
+          <th>ID</th>
           <th>Name</th>
-          <th>Description</th>
-          <th>Price</th>
           <th>Quantity</th>
+          <th>Category</th>
           <th>Actions</th>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>
+        <tr>
+          <td>0001</td>
+          <td>Pencil</td>
+          <td>10</td>
+          <td>Officer</td>
+          <td className="table-buttons">
+            <Link className="button is-small is-primary">Details</Link>
+            <Link className="button is-small">Update</Link>
+          </td>
+        </tr>
+      </tbody>
     </table>
   )
 }
